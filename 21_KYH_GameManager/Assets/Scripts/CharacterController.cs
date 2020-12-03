4dfe143c8bc4572e1c8f9210ffcdb9f1c6f78cb8
YeowHwee <yeowhwee13@gmail.com>
2020-12-03 22:34:53 +0800

    yes

---
+++ 21_KYH_GameManager/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterController : MonoBehaviour
+{
+    public float speed;
+    public Animator animator;
+
+    public GameObject EnergyCollected;
+    private int energyCount;
+    private int totalEnergy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        totalEnergy = GameObject.FindGameObjectsWithTag("addEnergyPrefab").Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            //startRun();
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+            //startRun();
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            transform.rotation = Quaternion.Euler(0, -90, 0);
+            //startRun();
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            transform.rotation = Quaternion.Euler(0, 90, 0);
+            //startRun();
+        }
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("addEnergyPrefab"))
+        {
+            energyCount += 5;
+            EnergyCollected.GetComponent<Text>().text = "Energy Count: " + energyCount;
+
+            Destroy(collision.gameObject);
+        }
+    }
+}
---
+++ 21_KYH_GameManager/Assets/Scripts/GameManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public GameObject addEnergyPrefab;
+    public GameObject minusEnergyPrefab;
+    public int numberOfSpawn;
+    public float levelTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        for (int i = 0; i < numberOfSpawn; i++)
+        {
+            Vector3 randomPos = new Vector3(Random.Range(-15, 15),
+                0,
+                Random.Range(-15, 15));
+
+            if (Random.Range(0, 2) < 1)
+            {
+                Instantiate(addEnergyPrefab, randomPos, Quaternion.identity);
+
+            }
+            else
+            {
+                Instantiate(minusEnergyPrefab, randomPos, Quaternion.identity);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Checking if there is still time left
+        if (levelTime > 0)
+        {
+            levelTime -= Time.deltaTime;
+            //print("levelTime: " + levelTime);
+            print("levelTime: " + FormatTime(levelTime));
+        }
+        else
+        {
+            levelTime = 0;
+            print("Times up!");
+        }
+    }
+
+    public string FormatTime(float time)
+    {
+        int minutes = (int)time / 60;
+        int seconds = (int)time / 60 * minutes;
+        int milliseconds = (int)(1000 * (time - minutes * 60 - seconds));
+        return string.Format("{0:00}:(1:00}:(2:000}", minutes, seconds, milliseconds);
+    }
+}
